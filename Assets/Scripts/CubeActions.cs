108cc71d7a5249e82b05bfee299e4ee1c1ff56a5
Ayushi Tandom <ayushitandon@icloud.com>
2020-10-14 12:16:36 -0500

    first commit for virtual reality

---
+++ Assets/Scripts/CubeActions.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeActions : MonoBehaviour
+{
+    bool moveRB = false;
+    bool moveEnter = false;
+    bool rotateRB = false;
+    bool rotateEnter = false;
+
+    void Update()
+    {
+        if(moveRB && moveEnter) {
+            MoveBoxRight();
+        }
+        if(rotateRB && rotateEnter) {
+            RotateBoxRight();
+        }
+
+    }
+
+    public void MoveBoxRightClickDown() {
+        moveRB = true;
+    }
+
+    public void MoveBoxRightClickUp() {
+        moveRB = false;
+    }
+
+    public void MoveBoxEnter() {
+        moveEnter = true;
+    }
+
+    public void MoveBoxExit() {
+        moveEnter = false;
+    }
+
+    public void MoveBoxRight() {
+        transform.position += new Vector3(1f, 0f, 0f);
+    }
+
+    public void RotateBoxRightClickDown() {
+        rotateRB = true;
+    }
+
+    public void RotateBoxRightClickUp() {
+        rotateRB = false;
+    }
+    public void RotateBoxEnter() {
+        rotateEnter = true;
+    }
+
+    public void RotateBoxExit() {
+        rotateEnter = false;
+    }
+
+    public void RotateBoxRight() {
+        transform.Rotate(1.0f, 0.0f, 0.0f, Space.Self);
+    }
+
+    public void ChangeBoxColorRed() {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# VirtualReality
+# CS6334Homework
+
+Unity 2019.3.15f1
+GVR SDK for Unity v1.200.1
